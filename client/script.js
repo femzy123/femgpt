9399f4332bac468e38bf7d2f23466b962edf03fc
femzy123 <femzy123@yahoo.com>
2022-12-29 10:59:38 +0100

    add favicon

--- client/script.js
+++ client/script.js
@@ -101,7 +101,7 @@ const handleSubmit = async (e) => {
 };
 
 form.addEventListener("submit", handleSubmit);
-form.addEventListener("keyup", (e) => {
+form.addEventListener("keyup", (e) => { 
   if (e.keyCode === 13) {
     handleSubmit(e);
   }
